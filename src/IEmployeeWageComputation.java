2fdecd62316fa165cbf0628bc03a499134d83d0d
shilpa123 <shilpabiradar100@gmail.com>
2023-02-17 23:51:38 +0530

    UC11/interface_and_implemented_in_empwagecomputation_class

---
+++ src/IEmployeeWageComputation.java
@@ -0,0 +1,5 @@
+public interface IEmployeeWageComputation {
+    public void addCompany(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs);
+
+    public void calculateTotalWage();
+}
